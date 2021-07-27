baef045abed1228a67fcc7e04959eb6f7fdb6d20
kevinmisati <kevinmosigisi2001@gmail.com>
2021-07-27 19:10:18 +0300

    dk

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,7 +5,7 @@ const Banner = () => {
     return (
         <div className={classes["banner-container"]}>
             <div className={classes.banner}>
-                <h2>All Your favourite electronics at one place  <button onClick={() => console.log("hello")}>hello</button></h2>
+                <h2>All Your favourite electronics at one place </h2>
                
                 {/* <a href='https://www.freepik.com/photos/computer'>Computer photo created by mego-studio - www.freepik.com</a> */}
             </div>
